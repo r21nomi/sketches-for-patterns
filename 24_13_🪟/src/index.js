6b9c034f18ca506799ec3083e7a6ca74e7eabf6e
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2022-06-12 09:11:43 +0900

    Add 24_13_🪟

---
+++ 24_13_🪟/src/index.js
@@ -0,0 +1,422 @@
+import * as THREE from "three";
+const vertexShader = require('webpack-glsl-loader!./shader/vertexShader.vert');
+const fragmentShader = require('webpack-glsl-loader!./shader/fragmentShader.frag');
+
+const clock = new THREE.Clock();
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(1.0, 1.0, 1.0);
+
+const MAX_AGE = 7;
+const duration = 12.0;
+const geometry = new THREE.BufferGeometry();
+
+const index = [];
+const vertices = [];
+const uvs = [];
+const offsets = [];
+const indices = [];
+const paddings = [];
+const colors = [];
+const size = [];
+const directions = [];
+const ratios = [];
+
+let baseTile;
+let totalRenderCount = 0;
+
+const uniforms = {
+    rect: {
+        time: { type: "f", value: 1.0 },
+        resolution: { type: "v2", value: new THREE.Vector2() },
+        uWidth: {
+            value: 1,
+        },
+        uHeight: {
+            value: 1,
+        },
+        duration: { type: "f", value: duration }
+    },
+    bg: {
+        time: { type: "f", value: 1.0 },
+        resolution: { type: "v2", value: new THREE.Vector2() }
+    }
+};
+
+// For dev
+let currentTime = [0];
+let imageIndex = 0;
+let isImageGenerationMode = false;
+let showGenerateImageButton = false;
+const span = 0.01;
+let timeoutSpan = 100;
+
+const map = (value, beforeMin, beforeMax, afterMin, afterMax) => {
+    return afterMin + (afterMax - afterMin) * ((value - beforeMin) / (beforeMax - beforeMin));
+}
+
+const objectGroup = new THREE.Group();
+
+// Camera
+const fov = 45;
+const aspect = window.innerWidth / window.innerHeight;
+const camera = new THREE.PerspectiveCamera(fov, aspect, 1, 10000);
+const stageHeight = window.innerHeight;
+// Make camera distance same as actual pixel value.
+const z = stageHeight / Math.tan(fov * Math.PI / 360) / 2;
+camera.position.z = z;
+
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+
+const render = () => {
+    const delta = clock.getDelta();
+    const time = clock.elapsedTime;
+    if (!isImageGenerationMode) {
+        currentTime[0] = time;
+    }
+
+    uniforms.rect.time.value = currentTime[0];
+    uniforms.bg.time.value = currentTime[0];
+
+    if (baseTile) {
+        baseTile.update();
+    }
+
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    renderer.render(scene, camera);
+
+    if (!isImageGenerationMode && !showGenerateImageButton) {
+        requestAnimationFrame(render);
+    }
+};
+
+const onResize = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    setSize(width, height);
+};
+
+const setSize = (width, height) => {
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+};
+
+const createImage = (canvas, fileName, index) => {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    let indexText = index;
+    if (index < 10) {
+        indexText = `000${index}`;
+    } else if (index < 100) {
+        indexText = `00${index}`;
+    } else if (index < 1000) {
+        indexText = `0${index}`;
+    }
+    link.href = image;
+    link.download = `${fileName}_${indexText}.png`;
+    link.click();
+};
+
+const generateImage = () => {
+    const canvas = document.querySelector("canvas");
+    render();
+    createImage(canvas, "img_1", imageIndex);
+
+    if (currentTime[0] !== 0 && currentTime[0] > duration) {
+        console.log("fin");
+        return;
+    }
+
+    currentTime[0] += span;
+    setTimeout(() => {
+        imageIndex++;
+        render();
+        generateImage();
+    }, timeoutSpan);
+};
+
+const generateImageButton = document.getElementById("generateImageButton");
+if (showGenerateImageButton) {
+    generateImageButton.style.display = "block";
+    generateImageButton.addEventListener("click", () => {
+        currentTime[0] = 0;
+        isImageGenerationMode = true;
+        generateImage();
+    });
+} else {
+    generateImageButton.remove();
+}
+
+const renderTiles = () => {
+    geometry.setIndex(indices);
+    geometry.setAttribute('index', new THREE.Uint16BufferAttribute(index, 1));
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+    geometry.setAttribute('uv', new THREE.Uint16BufferAttribute(uvs, 2));
+    geometry.setAttribute('size', new THREE.Float32BufferAttribute(size, 2));
+    geometry.setAttribute('offset', new THREE.Float32BufferAttribute(offsets, 2));
+    geometry.setAttribute('padding', new THREE.Float32BufferAttribute(paddings, 2));
+    geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
+    geometry.setAttribute('direction', new THREE.Float32BufferAttribute(directions, 1));
+    geometry.setAttribute('ratio', new THREE.Float32BufferAttribute(ratios, 1));
+
+    const material = new THREE.RawShaderMaterial({
+        uniforms: uniforms.rect,
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        transparent: true,
+        blending: THREE.NormalBlending,
+        depthTest: true,
+        wireframe: false,
+        glslVersion: THREE.GLSL1
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    objectGroup.add(mesh);
+
+    scene.add(objectGroup);
+};
+
+const createTiles = () => {
+    baseTile = new Tile(-window.innerWidth / 2, -window.innerHeight / 2, window.innerWidth, window.innerHeight, 0);
+    renderTiles();
+};
+
+class Tile {
+    constructor(x, y, w, h, age) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.age = age;
+        this.children = [];
+        this.offset = Math.floor(Math.random() * 50 + 1);
+        this.ratio = Math.random();
+        this.targetRatio = this.ratio;
+        this.shouldRender = false;
+        this.id = -1;
+        this.impulse = 0;
+
+        if (this.age < MAX_AGE) {
+            const nextAge = this.age + 1;
+            if (this.age % 2 === 0) {
+                // horizontal
+                // ||
+                const w1 = this.w * this.ratio;
+                const w2 = this.w * (1 - this.ratio);
+                this.children[0] = new Tile(
+                    this.x,
+                    this.y,
+                    w1,
+                    this.h, nextAge
+                );
+                this.children[1] = new Tile(
+                    this.x + w1,
+                    this.y,
+                    w2,
+                    this.h,
+                    nextAge
+                );
+            } else {
+                // vertical
+                // ＝
+                const h1 = this.h * this.ratio;
+                const h2 = this.h * (1 - this.ratio);
+                this.children[0] = new Tile(
+                    this.x,
+                    this.y,
+                    this.w,
+                    h1,
+                    nextAge
+                );
+                this.children[1] = new Tile(
+                    this.x,
+                    this.y + h1,
+                    this.w,
+                    h2,
+                    nextAge
+                );
+            }
+        } else {
+            // for render
+            this.draw(false);
+        }
+    }
+    update(arg = null) {
+        if (!!arg) {
+            this.x = arg.x;
+            this.y = arg.y;
+            this.w = arg.w;
+            this.h = arg.h;
+            this.impulse = arg.impulse;
+        }
+        if (this.children.length > 0) {
+            const ratioDiff = Math.abs(this.ratio - this.targetRatio);
+            if (Math.abs(this.ratio - this.targetRatio) < 0.001) {
+                this.targetRatio = Math.random();
+            }
+            const r = Math.max(Math.min(Math.abs(this.targetRatio - this.ratio) * 2, 0.04), 0.0);
+            this.ratio += (this.targetRatio - this.ratio) * r;
+
+            if (this.age % 2 === 0) {
+                // horizontal
+                // ||
+                const x1 = this.x;
+                const y1 = this.y;
+                const w1 = this.w * this.ratio;
+                const h1 = this.h;
+                this.children[0].update({
+                    x: x1,
+                    y: y1,
+                    w: w1,
+                    h: h1,
+                    impulse: ratioDiff
+                });
+
+                const x2 = x1 + w1;
+                const y2 = y1;
+                const w2 = this.w * (1 - this.ratio);
+                const h2 = this.h;
+                this.children[1].update({
+                    x: x2,
+                    y: y2,
+                    w: w2,
+                    h: h2,
+                    impulse: ratioDiff
+                });
+            } else {
+                // vertical
+                // ＝
+                const x1 = this.x;
+                const y1 = this.y;
+                const w1 = this.w;
+                const h1 = this.h * this.ratio;
+                this.children[0].update({
+                    x: x1,
+                    y: y1,
+                    w: w1,
+                    h: h1,
+                    impulse: ratioDiff
+                });
+
+                const x2 = this.x;
+                const y2 = this.y + h1;
+                const w2 = this.w;
+                const h2 = this.h * (1 - this.ratio);
+                this.children[1].update({
+                    x: x2,
+                    y: y2,
+                    w: w2,
+                    h: h2,
+                    impulse: ratioDiff
+                });
+            }
+        } else {
+            // render
+            this.draw(true);
+        }
+    }
+    draw(shouldUpdate = false) {
+        this.shouldRender = true;
+
+        if (shouldUpdate) {
+            // Update
+            for (let j = 0; j < 4; j++) {
+                const targetIndex = this.id * 4 + j;
+
+                const position = geometry.attributes.position;
+                position.setXYZ(targetIndex, this.x, this.y, 0);
+                position.needsUpdate = true;
+
+                const size = geometry.attributes.size;
+                size.setXY(targetIndex, this.w, this.h);
+                size.needsUpdate = true;
+
+                const ratio = geometry.attributes.ratio;
+                ratio.setX(targetIndex, this.impulse);
+                ratio.needsUpdate = true;
+
+                const direction = geometry.attributes.direction;
+                direction.setX(targetIndex, this.getDirection());
+                direction.needsUpdate = true;
+            }
+        } else {
+            // Initial
+            this.id = totalRenderCount;
+
+            for (let j = 0; j < 4; j++) {
+                vertices.push(this.x, this.y, 0);
+                size.push(this.w, this.h);
+                directions.push(this.getDirection());
+                ratios.push(this.ratio);
+            }
+
+            const color = {
+                x: map(Math.random(), 0.0, 1.0, 0.7, 0.8),
+                y: map(Math.random(), 0.0, 1.0, 0.1, 0.3),
+                z: map(Math.random(), 0.0, 1.0, 0.1, 0.3),
+            };
+
+            const padding = 4.0;
+
+            for (let j = 0; j < 4; j++) {
+                index.push(this.id);
+                paddings.push(padding, padding);
+                colors.push(color.x, color.y, color.z);
+            }
+
+            uvs.push(
+                0, 0,
+                1, 0,
+                1, 1,
+                0, 1
+            );
+            // offsets.push(
+            //     -1, -1,
+            //     1, -1,
+            //     1, 1,
+            //     -1, 1
+            // );
+            offsets.push(
+                0, 0,
+                1, 0,
+                1, 1,
+                0, 1
+            );
+
+            // polygon order
+            // 3 -- 2
+            // |    |
+            // 0 -- 1
+            const vertexIndex = this.id * 4;
+            indices.push(
+                vertexIndex + 0, vertexIndex + 1, vertexIndex + 2,
+                vertexIndex + 2, vertexIndex + 3, vertexIndex + 0
+            );
+
+            totalRenderCount++;
+        }
+
+        // console.log(`for render: x: ${this.x}, y: ${this.y}, w: ${this.w}, h: ${this.h}`);
+    }
+    getDirection() {
+        if (Math.abs(this.w - this.h) < 100.0) {
+            return -1.0;
+        } else if (this.w > this.h) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
+}
+
+createTiles();
+
+if (!showGenerateImageButton) {
+    window.addEventListener("resize", onResize);
+}
+
+onResize();
+render();
---
+++ 24_13_🪟/src/shader/fragmentShader.frag
@@ -0,0 +1,287 @@
+precision mediump float;
+
+const float PI = 3.1415926535897932384626433832795;
+
+uniform vec2 resolution;
+uniform float time;
+
+varying float vIndex;
+varying vec2 vUv;
+varying vec3 vColor;
+varying vec2 vResolution;
+varying float vDirection;
+varying float vRatio;
+
+mat2 rotate(float angle) {
+    return mat2(
+        sin(angle), -cos(angle),
+        cos(angle), sin(angle)
+    );
+}
+
+float ease_in_out_expo(float x) {
+    float t=x; float b=0.; float c=1.; float d=1.;
+    if (t==0.) return b;
+    if (t==d) return b+c;
+    if ((t/=d/2.) < 1.) return c/2. * pow(2., 10. * (t - 1.)) + b;
+    return c/2. * (-pow(2., -10. * --t) + 2.) + b;
+}
+
+float rect(vec2 size, vec2 uv, float edge) {
+    vec2 d = abs(uv - vec2(0.0)) - size + edge;
+    float l = length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
+    return 1.0 - step(l - edge, 0.0);
+}
+
+vec3 edgeStripe(vec3 edgeColor, vec2 uv, vec2 uvAspect) {
+    vec3 c = mix(edgeColor * 0.0, edgeColor, step(0.4, fract(uv.y * 30.0)));
+    c = mix(c, edgeColor, step(uvAspect.x - 0.04, length(uv.x)));
+    c = mix(c, edgeColor, step(uvAspect.y - 0.04, length(uv.y)));
+    return c;
+}
+
+float circle(float size, vec2 uv) {
+    return step(size, length(uv));
+}
+
+float borderCircle(float size, vec2 uv, float boldness) {
+    float c1 = circle(size, uv);
+    float c2 = circle(size - boldness, uv);
+    return abs(c1 - c2);
+}
+
+float _borderRect(vec2 size, vec2 uv, float boldness, float edge) {
+    float r1 = rect(size, uv, edge);
+    float r2 = rect(size - boldness, uv, edge);
+    float border = abs(r1 - r2);
+    return border;
+}
+
+vec3 borderRect(vec3 color, vec3 borderColor, vec2 size, vec2 uv, float boldness, float edge) {
+    float border = _borderRect(size, uv, boldness, edge);
+    color = mix(color, borderColor, border);
+    return color;
+}
+
+vec3 borderBtn(vec2 uv, vec3 color, vec3 bgColor, vec2 size, float boldness) {
+    float border = _borderRect(size, uv, boldness, 0.0);
+    color = mix(color, bgColor, border);
+    return color;
+}
+
+vec3 btn(vec2 uv, vec3 color, vec3 bgColor, vec2 size) {
+    float _rect = rect(size, uv, 0.0);
+    color = mix(bgColor, color, _rect);
+    return color;
+}
+
+vec3 windowContent(vec2 uv, vec3 color) {
+    uv = fract(uv * 20.0);
+    uv -= 0.5;
+    color *= step(0.15, length(uv));
+    return color;
+}
+
+void main() {
+    vec2 uv = (vUv.xy * vResolution * 2.0 - vResolution.xy) / min(vResolution.x, vResolution.y);
+    vec2 uvAspect = vResolution / min(vResolution.x, vResolution.y);
+
+    float id = floor(mod(vIndex, 4.0));
+    if (id == 0.0) {
+        id = 1.0;
+    }
+
+    vec3 color = vec3(1.0);
+    vec2 dir = vec2(0.0);
+
+    if (vDirection > 0.5) {
+        // vertical
+        dir = vec2(1.0, 0.0);
+    } else {
+        // horizontal
+        dir = vec2(0.0, 1.0);
+    }
+
+    vec3 color1 = vec3(251.0, 251.0, 251.0) / 255.0;
+    vec3 color2 = vec3(251.0, 251.0, 251.0) / 255.0;
+    vec3 color3 = vec3(251.0, 251.0, 251.0) / 255.0;
+    vec3 color4 = vec3(251.0, 251.0, 251.0) / 255.0;
+    vec3 bgColor = vec3(1.0);
+    vec3 objColor = vec3(1.0);
+    vec3 edgeTopColor = vec3(1.0);
+    vec3 edgeBottomColor = vec3(1.0);
+    vec3 edgeColor = mix(edgeTopColor, edgeBottomColor, (length(vUv.x * 2.0 - 1.0)));
+    vec3 windowBorderColor = vec3(0.0);
+    vec3 buttonBorderColor = vec3(0.0);
+    vec3 lastEdgeColor = edgeColor;
+    vec3 closeButtonBGColor = vec3(1.0);
+    vec3 miximizingButtonBGColor = vec3(1.0);
+    vec3 minimizingButtonBGColor = vec3(1.0);
+    vec3 closeButtonColor = vec3(0.25);
+    vec2 closeButton = vec2(0.0);
+    vec2 lastCloseButton = vec2(0.0);
+    vec2 maximizingButton = vec2(0.0);
+    vec2 lastMaximizingButton = vec2(0.0);
+    vec2 minimizingButton = vec2(0.0);
+    vec2 lastMinimizingButton = vec2(0.0);
+    float edgeSize = 0.2;
+    float t = (time + vIndex) * 0.6;
+    float easing = ease_in_out_expo(fract(t));
+    float easedTime = floor(t) + easing;
+    float _speed = 1.0;
+    float idTime = easedTime + 1.0 * _speed * id;
+    float ballPosition = mod(idTime, 4.0);
+    vec2 ballSize = vec2(0.0);
+    float margin = 0.0;
+    vec2 maxMove = (uvAspect - margin) - ballSize;
+    vec2 move = (fract(idTime) * 2.0 - 1.0) * maxMove;
+    vec2 dd = vec2(1.0);
+    bool isVertical = false;
+    float edge = 0.0;
+    float lastEdge = 0.0;
+    vec2 lastBallSize = maxMove;
+    float windowBorderBoldness = 0.01;
+    float buttonBorderBoldness = 0.014;
+    float buttonSize = 0.04;
+    float windowRadius = 0.0;
+    float buttonRadius = 0.015;
+    float edgeY = 0.0;
+    float edgeBottomPadding = 0.24;
+    float edgeBottomBorderBoldness = 0.005;
+
+    if (ballPosition < 1.0) {
+        // left
+        isVertical = true;
+        ballSize.x += easing * maxMove.x;
+        dd = vec2(move.x - ballSize.x, 0.0);
+        ballSize.y = maxMove.y;
+        edgeY = uv.y - maxMove.y;
+        edge = 1.0 - step(-edgeSize, edgeY);
+        lastEdge = edge;
+        closeButton = vec2(uv.x + move.x, uv.y - maxMove.y);
+        lastCloseButton = vec2(uv.x + uvAspect.x * 1.0, uv.y - maxMove.y);
+        maximizingButton = closeButton;
+        lastMaximizingButton = lastCloseButton;
+        minimizingButton = vec2(uv.x + move.x - uvAspect.x * 2.0, uv.y - maxMove.y);
+        lastMinimizingButton = vec2(uv.x - uvAspect.x, uv.y + maxMove.y - uvAspect.y * 2.0);
+
+        bgColor = color2;
+        objColor = color3;
+        edgeColor = mix(edgeTopColor, edgeBottomColor, smoothstep(0.0, edgeSize, length(uv.y - maxMove.y)));
+    } else if (ballPosition < 2.0) {
+        // up
+        ballSize.y += easing * maxMove.y;
+        dd = vec2(0.0, -move.y + ballSize.y);
+        ballSize.x = maxMove.x;
+        edgeY = uv.y - move.y;
+        edge = 1.0 - step(-edgeSize, edgeY);
+        lastEdge = 1.0 - step(-edgeSize, uv.y - maxMove.y);
+        closeButton = vec2(uv.x + uvAspect.x * 1.0, uv.y - move.y);
+        lastCloseButton = vec2(uv.x + uvAspect.x * 1.0, uv.y - maxMove.y);
+        maximizingButton = closeButton;
+        lastMaximizingButton = lastCloseButton;
+        minimizingButton = vec2(uv.x - uvAspect.x, uv.y - move.y);
+        lastMinimizingButton = vec2(uv.x + maxMove.x - uvAspect.x * 2.0, uv.y - maxMove.y);
+
+        bgColor = color3;
+        objColor = color4;
+        edgeColor = mix(edgeTopColor, edgeBottomColor, smoothstep(0.0, edgeSize, length(uv.y - move.y)));
+    } else if (ballPosition < 3.0) {
+        // right
+        isVertical = true;
+        ballSize.x += easing * maxMove.x;
+        dd = vec2(-move.x + ballSize.x, 0.0);
+        ballSize.y = maxMove.y;
+        edgeY = uv.y - maxMove.y;
+        edge = 1.0 - step(-edgeSize, edgeY);
+        lastEdge = edge;
+        closeButton = vec2(uv.x - move.x + uvAspect.x * 2.0, uv.y - maxMove.y);
+        lastCloseButton = vec2(uv.x + uvAspect.x, uv.y - maxMove.y);
+        maximizingButton = closeButton;
+        lastMaximizingButton = lastCloseButton;
+        minimizingButton = vec2(uv.x - move.x, uv.y - maxMove.y);
+        lastMinimizingButton = vec2(uv.x - uvAspect.x, uv.y - maxMove.y);
+
+        bgColor = color4;
+        objColor = color1;
+        edgeColor = mix(edgeTopColor, edgeBottomColor, smoothstep(0.0, edgeSize, length(uv.y - maxMove.y)));
+    } else {
+        // down
+        ballSize.y += easing * maxMove.y;
+        dd = vec2(0.0, move.y - ballSize.y);
+        ballSize.x = maxMove.x;
+        edgeY = uv.y + move.y - uvAspect.y * 2.0;
+        edge = 1.0 - step(-edgeSize, edgeY);
+        lastEdge = 1.0 - step(-edgeSize, uv.y + maxMove.y - uvAspect.y * 2.0);
+        closeButton = vec2(uv.x + uvAspect.x * 1.0, uv.y + move.y - uvAspect.y * 2.0);
+        lastCloseButton = vec2(uv.x + uvAspect.x * 1.0, uv.y - maxMove.y);
+        maximizingButton = closeButton;
+        lastMaximizingButton = lastCloseButton;
+        minimizingButton = vec2(uv.x - uvAspect.x, uv.y + move.y - uvAspect.y * 2.0);
+        lastMinimizingButton = vec2(uv.x - maxMove.x, uv.y - maxMove.y);
+
+        bgColor = color1;
+        objColor = color2;
+        edgeColor = mix(edgeTopColor, edgeBottomColor, smoothstep(0.0, edgeSize, length(uv.y + move.y - uvAspect.y * 2.0)));
+    }
+
+    // last window
+    lastEdgeColor = edgeStripe(lastEdgeColor, uv, uvAspect);
+    bgColor = windowContent(uv, bgColor);
+    bgColor = mix(lastEdgeColor, bgColor, lastEdge);
+    color = mix(bgColor, color, rect(lastBallSize, uv, windowRadius));
+    color = borderRect(color, windowBorderColor, uvAspect, uv, windowBorderBoldness, windowRadius);
+    // border
+    color *= step(edgeBottomBorderBoldness, length(uv.y - maxMove.y + edgeBottomPadding));
+
+    vec2 closeButtonProp = vec2(0.05, 7.0);  // size, boldness
+
+    vec2 btnSize = vec2(edgeSize * 0.5) * vec2(1.0, 0.9);
+    vec2 btnBorderSize = vec2(edgeSize * 0.4) * vec2(1.0, 0.9);
+    float btnXOssset = 0.1;
+    float btnYOssset = 0.023;
+
+    // last close button
+    vec2 lastColseBtnPos = lastCloseButton + vec2(-edgeSize * 0.5 - btnXOssset, edgeSize * 0.5 + btnYOssset);
+    color = btn(lastColseBtnPos, color, closeButtonBGColor, btnSize);
+    color = borderBtn(lastColseBtnPos, color, buttonBorderColor, btnBorderSize, buttonBorderBoldness);
+
+    float offf = 0.4;
+    vec2 _minimizingPos = vec2(edgeSize * 0.5 + btnXOssset, edgeSize * 0.5 + btnYOssset);
+
+    // last minimizing button
+    vec2 lastMinimizingBtnPos = lastMinimizingButton + _minimizingPos;
+    color = btn(lastMinimizingBtnPos, color, minimizingButtonBGColor, btnSize);
+    color = borderBtn(lastMinimizingBtnPos, color, buttonBorderColor, btnBorderSize, buttonBorderBoldness);
+
+    // new window
+    edgeColor = edgeStripe(edgeColor, uv, uvAspect);
+    objColor = windowContent(uv + dd, objColor);
+    objColor = mix(edgeColor, objColor, edge);
+    float cc = rect(ballSize, uv + dd, windowRadius);
+    color = mix(objColor, color, cc);
+    color = borderRect(color, windowBorderColor, ballSize, uv + dd, windowBorderBoldness, windowRadius);
+    // border
+    color *= step(edgeBottomBorderBoldness, length(edgeY + edgeBottomPadding));
+
+    // new colose button
+    vec2 colseBtnPos = closeButton + vec2(-edgeSize * 0.5 - btnXOssset, edgeSize * 0.5 + btnYOssset);
+    color = btn(colseBtnPos, color, closeButtonBGColor, btnSize);
+    color = borderBtn(colseBtnPos, color, buttonBorderColor, btnBorderSize, buttonBorderBoldness);
+
+    // minimizing button
+    vec2 minimizingBtnPos = minimizingButton + _minimizingPos;
+    color = btn(minimizingBtnPos, color, minimizingButtonBGColor, btnSize);
+    color = borderBtn(minimizingBtnPos, color, buttonBorderColor, btnBorderSize, buttonBorderBoldness);
+
+    float borderWidth = 0.0;
+
+    float l = step(vResolution.x * vUv.x, borderWidth)
+        + step(vResolution.x - borderWidth, vResolution.x * vUv.x)
+        + step(vResolution.y *vUv.y, borderWidth)
+        + step(vResolution.y - borderWidth, vResolution.y * vUv.y);
+
+    float transparency = mix(1.0, 0.0, l);
+
+    gl_FragColor = vec4(color, transparency);
+}
