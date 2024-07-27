c4406388c03313a22c22b7a6f3b62d0f7d9bf580
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2024-07-27 18:04:19 +0900

    Fix error "digital envelope routines::unsupported"

--- 24_ recomposition/24_4_|||Ξ/webpack.config.js
+++ 24_ recomposition/24_4_|||Ξ/webpack.config.js
@@ -1,12 +1,28 @@
 const path = require("path");
+const webpack = require('webpack')
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "public")
+  mode: 'production',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      // Make generated files only 1
+      maxChunks: 1,
+    }),
+  ],
+  performance: {
+    maxEntrypointSize: 800000,
+    maxAssetSize: 800000,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    devServer: {
-        contentBase: path.join(__dirname, "public")
-    }
+    compress: true,
+    port: 8080,
+  },
 };
--- 25_typography/25_1_ripple/webpack.config.js
+++ 25_typography/25_1_ripple/webpack.config.js
@@ -1,12 +1,21 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "index.js",
-        path: path.resolve(__dirname, "public")
+  mode: 'production',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "public")
+  },
+  performance: {
+    maxEntrypointSize: 800000,
+    maxAssetSize: 800000,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
     },
-    devServer: {
-        contentBase: path.join(__dirname, "public")
-    }
+    compress: true,
+    port: 8080,
+  },
 };
